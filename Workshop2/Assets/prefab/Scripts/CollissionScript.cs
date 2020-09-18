133d48fefeb90c15ecfa074e704ff52ccab94970
hspaij <70332287@users.noreply.github.com>
2020-09-18 16:32:41 +0200

    final fixes (presentatie fixen + erna)

---
+++ Workshop2/Assets/prefab/Scripts/CollissionScript.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollissionScript : MonoBehaviour
+{
+    GameObject lightsource;
+    GameObject scissors;
+
+    void OnCollisionEnter(Collision col)
+    {
+	if (col.gameObject.name == "Lightsource") 
+	{
+		this.lightsource = col.gameObject;
+		this.lightsource.GetComponent<LightsourceMovement>().pickedUp = true;
+	}
+
+	if (col.gameObject.name == "Scissors") 
+	{
+		this.scissors = col.gameObject;
+		this.scissors.GetComponent<ScissorsMovement>().pickedUp = true;
+	}
+
+	if (col.gameObject.name == "LightsourceDestination")
+	{
+		this.lightsource.GetComponent<LightsourceMovement>().placeLightsource();
+	}
+    }
+}
+/*
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchPaint : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void OnCollisionEnter(Collision col)
+    {
+	GameObject[] walls;
+        Debug.Log(col.gameObject.name);
+
+	if (col.gameObject.name == "PaintRed") {
+            GetComponent<MeshRenderer>().material.color = Color.red;
+	    walls = GameObject.FindGameObjectsWithTag("RedWall");
+	    foreach(GameObject wall in walls) {
+		wall.active = false;
+	    }
+
+	    
+	    walls = GameObject.FindGameObjectsWithTag("YellowWall");
+            foreach(GameObject wall in walls) {
+		wall.active = true;
+	    }
+		
+		
+	    walls = GameObject.FindGameObjectsWithTag("BlueWall");
+            foreach(GameObject wall in walls) {
+		wall.active = true;
+	    }
+
+
+	} else if (col.gameObject.name == "PaintBlue") {
+Debug.Log(123);
+            GetComponent<MeshRenderer>().material.color = Color.blue;
+	    walls = GameObject.FindGameObjectsWithTag("RedWall");
+	    foreach(GameObject wall in walls) {
+		wall.active = true;
+	    }
+
+	    
+	    walls = GameObject.FindGameObjectsWithTag("YellowWall");
+            foreach(GameObject wall in walls) {
+		wall.active = true;
+	    }
+		
+		
+	    walls = GameObject.FindGameObjectsWithTag("BlueWall");
+            foreach(GameObject wall in walls) {
+		wall.active = false;
+	    }
+	} else if (col.gameObject.name == "PaintYellow") {
+            GetComponent<MeshRenderer>().material.color = Color.yellow;
+	    walls = GameObject.FindGameObjectsWithTag("RedWall");
+	    foreach(GameObject wall in walls) {
+		wall.active = true;
+	    }
+
+	    
+	    walls = GameObject.FindGameObjectsWithTag("YellowWall");
+            foreach(GameObject wall in walls) {
+		wall.active = false;
+	    }
+		
+		
+	    walls = GameObject.FindGameObjectsWithTag("BlueWall");
+            foreach(GameObject wall in walls) {
+		wall.active = true;
+	    }
+	}
+    }
+}
+*/
---
+++ Workshop2/Assets/prefab/Scripts/LightsourceMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightsourceMovement : MonoBehaviour
+{
+    public bool pickedUp = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         GameObject player = GameObject.FindWithTag("Player");
+	 if(this.pickedUp == true) {
+		transform.position = new Vector3(
+			player.transform.position.x - 1, 
+			player.transform.position.y,
+			player.transform.position.z);
+	  }
+    }
+
+    public void placeLightsource() {
+	this.pickedUp = false;
+	GameObject finish = GameObject.FindWithTag("Finish");
+	transform.position = new Vector3(
+		finish.transform.position.x - 1, 
+		finish.transform.position.y,
+		finish.transform.position.z);
+    }
+}
